09d0d8ae9c087ce26e376e714f491a41d82c4729
CongDon1207 <22133013@student.hcmute.edu.vn>
2024-10-18 14:34:00 +0700

    new project

---
+++ TheGioiTho/Config/DBConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace TheGioiTho.Config
+{
+    public static class DBConnection
+    {
+        // Chuỗi kết nối tới cơ sở dữ liệu từ Settings
+        private static readonly string connectionString = Properties.Settings.Default.conn;
+
+        // Phương thức tạo kết nối tới cơ sở dữ liệu
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
