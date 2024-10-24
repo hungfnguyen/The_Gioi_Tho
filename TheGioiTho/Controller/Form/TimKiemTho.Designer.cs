974a031a785453b4784156037616154436fd6591
nnhhlkj123 <nguyenngochieuhao@gmail.com>
2024-10-24 08:22:14 +0700

    update ver1 1.4-1.7

--- TheGioiTho/Controller/Form/TimKiemTho.Designer.cs
+++ TheGioiTho/Controller/Form/TimKiemTho.Designer.cs
@@ -32,9 +32,25 @@
             this.btnTimKiem = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridViewTimKiem = new System.Windows.Forms.DataGridView();
-            this.XemChiTietCongViec = new System.Windows.Forms.DataGridViewButtonColumn();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.XemChiTiet = new System.Windows.Forms.DataGridViewButtonColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTimKiem)).BeginInit();
             this.SuspendLayout();
             // 
@@ -70,21 +86,14 @@
             this.dataGridViewTimKiem.BackgroundColor = System.Drawing.Color.White;
             this.dataGridViewTimKiem.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridViewTimKiem.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.XemChiTietCongViec});
+            this.XemChiTiet});
             this.dataGridViewTimKiem.Location = new System.Drawing.Point(58, 67);
             this.dataGridViewTimKiem.Name = "dataGridViewTimKiem";
             this.dataGridViewTimKiem.RowHeadersWidth = 51;
             this.dataGridViewTimKiem.RowTemplate.Height = 24;
-            this.dataGridViewTimKiem.Size = new System.Drawing.Size(1057, 443);
+            this.dataGridViewTimKiem.Size = new System.Drawing.Size(1057, 431);
             this.dataGridViewTimKiem.TabIndex = 0;
-            // 
-            // XemChiTietCongViec
-            // 
-            this.XemChiTietCongViec.HeaderText = "";
-            this.XemChiTietCongViec.MinimumWidth = 6;
-            this.XemChiTietCongViec.Name = "XemChiTietCongViec";
-            this.XemChiTietCongViec.Text = "XemChiTietCongViec";
-            this.XemChiTietCongViec.Width = 125;
+            this.dataGridViewTimKiem.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewTimKiem_CellContentClick);
             // 
             // button1
             // 
@@ -106,11 +115,169 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1128, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Tiêu Đề";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1128, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Mô Tả";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1128, 180);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Hình Ảnh";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1128, 236);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Tên Lĩnh Vực";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1128, 283);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 16);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Họ Tên Thợ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1128, 339);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(58, 16);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Giá Tiền";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(1128, 389);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(129, 16);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Thời Gian Thực Hiện";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1263, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(262, 22);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1263, 119);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(262, 22);
+            this.textBox2.TabIndex = 14;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1263, 180);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(262, 22);
+            this.textBox3.TabIndex = 15;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1263, 236);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(262, 22);
+            this.textBox4.TabIndex = 16;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(1263, 283);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(262, 22);
+            this.textBox5.TabIndex = 17;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(1263, 339);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(262, 22);
+            this.textBox6.TabIndex = 18;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(1263, 389);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(266, 22);
+            this.textBox7.TabIndex = 19;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(1128, 444);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(0, 16);
+            this.label9.TabIndex = 22;
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1275, 454);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(153, 44);
+            this.button3.TabIndex = 24;
+            this.button3.Text = "Đặt Lịch";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // XemChiTiet
+            // 
+            this.XemChiTiet.HeaderText = "XemChiTiet";
+            this.XemChiTiet.MinimumWidth = 6;
+            this.XemChiTiet.Name = "XemChiTiet";
+            this.XemChiTiet.Text = "XemChiTiet";
+            this.XemChiTiet.Width = 125;
+            // 
             // TimKiemTho
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1155, 522);
+            this.ClientSize = new System.Drawing.Size(1621, 520);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
@@ -131,9 +298,25 @@
         private System.Windows.Forms.Button btnTimKiem;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridViewTimKiem;
-        private System.Windows.Forms.DataGridViewButtonColumn XemChiTietCongViec;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewButtonColumn XemChiTiet;
     }
 }
 
--- TheGioiTho/Controller/Form/TimKiemTho.cs
+++ TheGioiTho/Controller/Form/TimKiemTho.cs
@@ -82,5 +82,52 @@ namespace TheGioiTho
             DanhSachThoYeuThich danhSachThoYeuThich = new DanhSachThoYeuThich();
             danhSachThoYeuThich.Show();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridViewTimKiem_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Kiểm tra nếu cột đầu tiên (index 0) được click
+            if (dataGridViewTimKiem.Columns[e.ColumnIndex].Name=="XemChiTiet")
+            {
+                // Lấy hàng hiện tại từ vị trí click
+                DataGridViewRow row = dataGridViewTimKiem.Rows[e.RowIndex];
+                {
+                    // Lấy dữ liệu từ các cột khác trong hàng đó              
+                    textBox1.Text = row.Cells[2].Value?.ToString(); // Cột thứ 2 (index 1)
+                    textBox2.Text = row.Cells[3].Value?.ToString(); // Cột thứ 3 (index 2)
+                    textBox3.Text = row.Cells[4].Value?.ToString(); // Cột thứ 4 (index 3)
+                    textBox4.Text = row.Cells[5].Value?.ToString();
+                    textBox5.Text = row.Cells[6].Value?.ToString();
+                    textBox6.Text = row.Cells[7].Value?.ToString();
+                    textBox7.Text = row.Cells[8].Value?.ToString();
+                    // Thực hiện logic của bạn với dữ liệu đã lấy
+                    
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
